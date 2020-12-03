82241cedf314dac08f56f70f1e489634050282a9
jrmdel <jeremie.deletraz@gmail.com>
2020-12-03 01:04:31 +0100

    Color palette chosen and first design

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,134 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <HelloWorld/>
-    </v-main>
+    <v-app>
+        <v-img src="@/assets/cajina-mid.jpg" lazy-src="@/assets/cajina-min.jpg">
+            <v-container fluid>
+                <div class="mx-16 pa-16">
+                <v-row>
+                    <v-col cols="12">
+                        <v-card elevation="12" class="tertiary">
+                            <v-card-title>
+                                <span class="text-h1 primaryLight--text">Ingénieur Fullstack JS & Data</span>
+                            </v-card-title>
+                        </v-card>
+                    </v-col>
+                </v-row>
+                <!--
+                <v-row v-for="i in 9" :key="i">
+                    <v-col>
+                        <v-card>
+                            <v-card-title>Blabla</v-card-title>
+                            <v-card-text>
+                                {{i}}
+                                <p>azaza</p>
+                                <p>aalmma</p>
+                            </v-card-text>
+                            <v-card-actions>
+                                <v-spacer></v-spacer>
+                                <v-btn>HELP</v-btn>
+                            </v-card-actions>
+                        </v-card>
+                    </v-col>
+                </v-row>
+                -->
+                <v-row justify="space-between">
+                    <v-col cols="4">
+                        <v-card elevation="12" class="primary">
+                            <v-container>
+                                <v-row justify="center">
+                                    <v-col cols="auto">
+                                        <v-avatar class="mx-2" size="256">
+                                            <v-img 
+                                                src="https://images2.minutemediacdn.com/image/upload/c_crop,h_1025,w_1823,x_45,y_0/f_auto,q_auto,w_1100/v1602272367/shape/mentalfloss/633067-wikimedia_commons.png"
+                                            ></v-img>
+                                        </v-avatar>
+                                    </v-col>
+                                </v-row>
+                                <v-row>
+                                    <v-container>
+                                        <v-row justify="center">
+                                            <p class="text-h4 text-center white--text mx-3">Jeremie Deletraz</p>
+                                            <!--
+                                            <p class="text-h6 font-weight-regular text-center mx-3 tertiary--text">
+                                                Je suis un ingénieur <span class="font-weight-medium">curieux</span>, repsonsable et enthousiaste, diplômé d'informatique. 
+                                                Initialement destiné à évoluer dans le domaine paramédical, 
+                                                j'ai décidé de reprendre mes études à 22 ans. Je me suis découvert 
+                                                une passion pour la data, et une grande appétence pour le développement web JavaScript.
+                                            </p>
+                                            -->
+                                            <p>
+                                                <v-icon>mdi-medal</v-icon>
+                                                <span>Heavy-weight champion</span>
+                                            </p>
+                                        </v-row>
+                                    </v-container>
+                                </v-row>
+                            </v-container>
+                            <v-card-title class="white--text">Blabla
+                                <v-spacer></v-spacer>
+                                <v-btn icon>
+                                    <v-icon large>mdi-close</v-icon>
+                                </v-btn>
+                            </v-card-title>
+                            <v-card-text>
+                                <p>mop</p>
+                                <p v-for="j in 3" :key="j">plaa{{j}}aa</p>
+                            </v-card-text>
+                        </v-card>
+                    </v-col>
+                    <v-col>
+                            <v-card class="mb-6 primaryLight">
+                                <v-card-title class="text-h3 font-weight-light tertiary--text">A propos de moi</v-card-title>
+                                <v-card-text>
+                                    <p class="text-h6 font-weight-regular mx-3 tertiary--text">
+                                        Je suis un ingénieur <span class="font-weight-medium">curieux</span>, repsonsable et enthousiaste, diplômé d'informatique. 
+                                        Initialement destiné à évoluer dans le domaine paramédical, 
+                                        j'ai décidé de reprendre mes études à 22 ans. Je me suis découvert 
+                                        une passion pour la data, et une grande appétence pour le développement web JavaScript.
+                                    </p>
+                                </v-card-text>
+                            </v-card>
+                        
+                            <v-card class="primaryLight">
+                                <v-card-title class="text-h3 font-weight-light tertiary--text">Expériences</v-card-title>
+                                <v-card-text>
+                                    <v-timeline dense align-top>
+                                        <v-timeline-item small color="primary">
+                                            <v-card dark color="primary">
+                                                <v-card-title>Nokia Lannion</v-card-title>
+                                                <v-card-text class="tertiary--text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque reiciendis vero quo laboriosam dolores vel tenetur dicta harum, consequuntur aut consequatur reprehenderit eligendi maxime facilis quod nesciunt animi commodi eveniet!</v-card-text>
+                                            </v-card>
+                                        </v-timeline-item>
+                                        <v-timeline-item dark small color="secondary">
+                                            <v-card color="secondary">
+                                                <v-card-title>Ericsson Lannion</v-card-title>
+                                                <v-card-text>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque reiciendis vero quo laboriosam dolores vel tenetur dicta harum, consequuntur aut consequatur reprehenderit eligendi maxime facilis quod nesciunt animi commodi eveniet!</v-card-text>
+                                            </v-card>
+                                        </v-timeline-item>
+                                        <v-timeline-item small color="tertiary">
+                                            <v-card dark color="tertiary">
+                                                <v-card-title>La Lunetterie Annecy</v-card-title>
+                                                <v-card-text>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque reiciendis vero quo laboriosam dolores vel tenetur dicta harum, consequuntur aut consequatur reprehenderit eligendi maxime facilis quod nesciunt animi commodi eveniet!</v-card-text>
+                                            </v-card>
+                                        </v-timeline-item>
+                                    </v-timeline>
+                                </v-card-text>
+                            </v-card>
+                    </v-col>
+                </v-row>
+                </div>
+            </v-container>
+        </v-img>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
 export default {
-  name: 'App',
-
-  components: {
-    HelloWorld,
-  },
-
-  data: () => ({
-    //
-  }),
+    data: () => ({
+        //
+    }),
 };
 </script>
+
+<style lang="scss">
+    $icon-size: 256px;
+</style>
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,4 +4,31 @@ import Vuetify from 'vuetify/lib';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    theme: {
+        themes: {
+            light: {
+                /*
+                // First set
+                primary: "#6369D1",
+                secondary: "#60E1E0",
+                tertiary:"#31393C",
+                quaternary: "#4C2C69",
+                accent: "#FFDA22",
+                error:"#540B0E",
+                background: "#F5ECEB"
+                */
+                
+                // Second set
+                primary:"#548096",
+                primaryLight: "#93AEBC",
+                secondary:"#6D1A36",
+                tertiary: "#192C32",
+                quaternary:"#916953",
+                accent: "#F9E900",
+                warning:"#F6AE2D",
+                error:"#96031A",
+            }
+            
+        }
+    }
 });
