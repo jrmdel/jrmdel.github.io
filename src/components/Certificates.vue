776f4b7b53b814a53fc73e61e626dfbbf474091a
jrmdel <jeremie.deletraz@gmail.com>
2020-12-09 14:02:27 +0100

    Adjusted the certificates component

--- src/components/Certificates.vue
+++ src/components/Certificates.vue
@@ -4,16 +4,20 @@
             <v-row class="text-h4 font-weight-light tertiary--text">
                 <v-col>Certifications</v-col>
             </v-row>
-            <v-row align="center" class="px-4" v-for="(crt,i) in cert" :key="i">
+            <v-row align="center" class="px-2" v-for="(crt,i) in cert" :key="i">
                 <v-col cols="auto">
-                    <v-icon color="tertiary">mdi-certificate-outline</v-icon>
+                    <v-row class="my-n1" no-gutters align="center">
+                    <v-btn icon :disabled="!crt.link" :href="crt.link">
+                        <v-icon color="tertiary">mdi-certificate-outline</v-icon>
+                    </v-btn>
                     <span class="ml-2">{{crt.year}}</span>
+                    </v-row>
                 </v-col>
                 <v-col>
                     <span class="tertiary--text">{{crt.title}}</span>
                 </v-col>
-                <v-col cols="auto">
-                    <v-icon color="tertiary">mdi-chart-box-outline</v-icon>
+                <v-col v-if="crt.score" cols="auto">
+                    <v-icon small color="tertiary">mdi-chart-box-outline</v-icon>
                     <span class="ml-2">{{crt.score}}</span>
                 </v-col>
             </v-row>
@@ -26,7 +30,8 @@ export default {
     data: ()=>({
         cert: [
             {title: "TOEIC", score: "960/990", year: "2018"},
-            {title: "Certification Le Robert", score: "827/1000", year: "2018", link:""}
+            {title: "Le Robert", score: "829/1000", year: "2018", link:"https://examen.certification-le-robert.com/index.html#/certificationPublic/SPUB3VAK2G"},
+            {title: "Develop Your Cultural Intelligence", year:"2020", link:"https://www.futurelearn.com/certificates/10ayxkf"}
         ],
     })
 }
