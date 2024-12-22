b9606c569498b1f8bcc1ba81e59d41e8bab860fd
jrmdel <jeremie.deletraz@gmail.com>
2024-12-23 00:35:10 +0100

    feat: add downloading function

--- src/components/MyTitle.vue
+++ src/components/MyTitle.vue
@@ -18,17 +18,27 @@
         >
           <v-icon :color="iconColor"> mdi-at </v-icon>
         </v-btn>
-        <!-- <v-btn :color="buttonColor" fab large outlined @click="download">
-                    <v-icon :color="iconColor">
-                        mdi-download
-                    </v-icon>
-                </v-btn> -->
+        <v-btn
+          class="ml-2"
+          :color="buttonColor"
+          :loading="isDownloading"
+          fab
+          outlined
+          @click="download"
+        >
+          <v-icon :color="iconColor">
+            mdi-download
+          </v-icon>
+        </v-btn>
       </v-card-title>
     </v-card>
   </div>
 </template>
 
 <script>
+const PDF_NAME = 'cv-jeremie-deletraz.pdf';
+const PDF_URL = 'https://rivonglade.onthewifi.com/html-to-pdf/api/v1/convert';
+
 export default {
   props: {
     cardColor: {
@@ -69,6 +79,52 @@ export default {
         toReturn += `&body=Bonjour Jérémie,%0D%0A%0D%0AJe viens de parcourir ton site ${document.URL} et je souhaite que l'on échange.`;
         return toReturn;
       },
+    },
+  },
+  data: () => ({
+    isDownloading: false,
+  }),
+  methods: {
+    download() {
+      this.isDownloading = true;
+
+      const cleanHtml = this.cleanHtml();
+      const formData = new FormData();
+      formData.append('file', new Blob([cleanHtml], { type: 'text/html' }));
+
+      fetch(PDF_URL, {
+        method: 'POST',
+        body: formData,
+      })
+        .then(response => response.blob())
+        .then(blob => {
+          this.autoSaveFile(blob);
+        })
+        .catch(error => {
+          console.error('Error while downloading file:', error);
+        })
+        .finally(() => {
+          this.isDownloading = false;
+        });
+    },
+    cleanHtml() {
+      const cloned = document.querySelector('html').cloneNode(true);
+      const nodesToDelete = cloned.querySelectorAll('#no-pdf');
+      nodesToDelete.forEach(e => {
+        e.remove();
+      });
+      return cloned.outerHTML;
+    },
+    autoSaveFile(blob) {
+      const url = window.URL.createObjectURL(blob);
+      const a = document.createElement('a');
+      a.style.display = 'none';
+      a.href = url;
+      a.download = PDF_NAME;
+      document.body.appendChild(a);
+      a.click();
+      document.body.removeChild(a);
+      window.URL.revokeObjectURL(url);
     },
   },
 };
